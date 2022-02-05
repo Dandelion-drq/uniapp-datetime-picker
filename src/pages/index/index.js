e057e33241547dbdbe4ca54d4a15d8cc4bcf8805
dengrq <dandelion.drq@gmail.com>
2022-02-05 17:09:19 +0800

    完成演示逻辑

---
+++ src/pages/index/index.js
@@ -0,0 +1,41 @@
+import DateSelector from "@/components/dateSelector/index.vue";
+
+// 日期范围类型
+const DATE_TYPES = [
+  {
+    label: '年月日',
+    value: 1
+  },
+  {
+    label: '年月',
+    value: 2
+  },
+  {
+    label: '年',
+    value: 3
+  },
+  {
+    label: '年月日时分秒',
+    value: 4
+  },
+]
+
+export default {
+  components: {
+    DateSelector,
+  },
+  data() {
+    return {
+      dateTypes: DATE_TYPES, // 日期范围类型
+      selectDateType: 1, // 选中的日期类型
+    }
+  },
+  methods: {
+    onTapDateType(type) {
+      this.selectDateType = type;
+    },
+    onDateSelectorChange({ startDate, endDate }) {
+      console.log('onDateSelectorChange', startDate, endDate)
+    },
+  },
+};
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,21 +1,30 @@
 <template>
-	<view class="content">
-		<DateSelector/>
-	</view>
+  <view class="content">
+    <view class="filter-date">
+      <view class="filter-group">
+        <view class="filter-group-title">统计时间</view>
+        <view class="filter-group-options">
+          <view
+            class="filter-group-option-btn small"
+            :class="{ selected: selectDateType == item.value }"
+            v-for="(item, index) in dateTypes"
+            :key="index"
+            @tap="onTapDateType(item.value)"
+            >{{ item.label }}</view
+          >
+        </view>
+
+        <view class="date-seletor-wrapper">
+          <DateSelector
+            :mode="selectDateType"
+            @onChange="onDateSelectorChange"
+          />
+        </view>
+      </view>
+    </view>
+  </view>
 </template>
 
-<script>
-	import DateSelector from '@/components/dateSelector/index.vue'
+<script src="./index.js"></script>
 
-	export default {
-		components: {
-			DateSelector
-		}
-	}
-</script>
-
-<style scoped>
-.content {
-	margin-top: 50px;
-}
-</style>
+<style scoped src="./index.css"></style>
